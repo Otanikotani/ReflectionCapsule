4194c036b2745f75e2caffd7a5bdfc51431c1b9b
Otanikotani <otanikotani@gmail.com>
2012-02-25 12:24:25 +0400

    Added 'hello' main

---
+++ ReflectionCapsule/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Aplicom Q1 Application.
+ * <pre>
+ * Aplicom is registered trademark of Aplicom Oy.
+ *
+ * (c) Aplicom Oy 2011
+ *
+ * All rights reserved. Reproduction in whole or in part in any form is prohibited
+ * without the prior consent of the copyright owner. Aplicom Oy makes every effort
+ * to ensure that the information in this manual is correct, but accepts no
+ * liability for any errors or omissions.
+ *
+ * Aplicom Oy has a policy of continuous improvement. Therefore we reserve the
+ * right to make changes and improvements to any of the products described in this
+ * guide without prior notice. Due to these improvements, the contents of this
+ * manual are subject to change without notice.
+ *
+ * Aplicom Oy is not responsible for any loss of data, income or any consequential
+ * damage.
+ * </pre>
+ */
+/**
+ @file     main.cpp
+ @author   Andrey Artyukhov CONSTANT
+ @date     Feb 25, 2012
+ @brief    VERNE project \n
+
+ @par Definition
+
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hi" << std::endl;
+}
